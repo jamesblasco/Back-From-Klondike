db5be70876ef3a5b19c00948536756c8855e8a1c
belen <belen.g.i.a.r@gmail.com>
2018-05-04 12:30:07 +0200

    BLKAH

---
+++ GLstuff.h
@@ -0,0 +1,19 @@
+ 
+ 
+#include <stdlib.h>
+#include "glut.h"
+#include <stdio.h>
+#include <math.h>
+ 
+#define  ANCHO 800
+#define ALTO 800
+
+#define ESC 27
+
+// Prototipos de funciones
+GLuint LoadTexture(int, int, const char * filename );
+
+void displaytext(GLuint Thetexture);
+
+void init_GL_stuff();
+ 
